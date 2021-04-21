de0ec10901cc8ec194c9df66011914199fa1a3b6
MAYANKSHUKLA123 <ramkumarshukla719@gmail.com>
2021-04-21 23:57:56 +0530

    themeToggler

---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ThemeToggler from "./Component/themeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
